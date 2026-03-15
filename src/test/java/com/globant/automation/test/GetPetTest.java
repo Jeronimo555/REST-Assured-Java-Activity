0df0b2b0b6e8cadf56b5e2f28c6d2789dc99d31b
Jeronimo Acosta  Acevedo <jeronimo.acosta@globant.com>
2026-03-15 14:40:53 -0500

    Change BASEURL to petstore. Create GetPetTest and associated PetDTO

---
+++ src/test/java/com/globant/automation/test/GetPetTest.java
@@ -0,0 +1,40 @@
+package com.globant.automation.test;
+
+import com.globant.automation.config.TestRunner;
+import com.globant.automation.model.GetUserResponseDTO;
+import com.globant.automation.model.PetDTO;
+import com.globant.automation.model.UserDTO;
+import com.globant.automation.request.RequestBuilder;
+import io.restassured.RestAssured;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.testng.annotations.Test;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.testng.Assert.assertEquals;
+
+public class GetPetTest extends TestRunner {
+
+    @Test(testName = "Requirement 4: Consult data of a specific pet")
+    public void getPetstorePetTest() {
+        // Define the pet to search (ensure this user exists in the Petstore)
+        Integer pet_id = 10;
+        String pet_name = "doggie";
+
+        // The endpoint is /pet/{petId}
+        Response response = RequestBuilder.getRequest(getBaseurl(), "/pet/" + pet_id, getApikey());
+
+        //Deserialization.
+        PetDTO userResponse = response.as(PetDTO.class);
+
+        //We make a couple of assertions to make sure the response body aligns with our data.
+        assertEquals(response.getStatusCode(), 200, "The status code doesn't match.");
+        assertEquals(userResponse.getId(), pet_id, "The id should match");
+        assertEquals(userResponse.getName(), pet_name, "The id should match");
+
+        System.out.println("Pet found: " + userResponse.getName());
+
+    }
+}
